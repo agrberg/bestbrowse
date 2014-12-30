63cfc34cab9b6ce928864d3b9819a8b70c5462d4
Aaron Rosenberg <aarongrosenberg@gmail.com>
2014-12-30 17:50:36 +0200

    clean up js styling

--- extensions/chrome/background.js
+++ extensions/chrome/background.js
@@ -10,11 +10,11 @@ var reqListener = function() {
     };
 
     chrome.notifications.create('notify1', opt, function(id) {});
-    chrome.notifications.onClicked.addListener(function (notification){
+    chrome.notifications.onClicked.addListener(function(notification){
       var properties = {
         url: this.responseText
       };
-      chrome.tabs.create(properties, function (tab) {});
+      chrome.tabs.create(properties, function(tab) {});
     });
 
     console.log(this.responseText);
@@ -48,33 +48,43 @@ chrome.runtime.onInstalled.addListener(function(details) {
   if (details.reason == "install") {4
     console.log("This is a first install!");
     chrome.history.search({text: "", maxResults: 2147483647}, function(historyItems) {
-      var myset = {};
-      var index;
+      var myset = {},
+          index,
+          unique_urls,
+          url;
+
       for (index = 0; index < historyItems.length; index++) {
         historyItem = historyItems[index];
         myset[historyItem.url] = true;
       }
-      var unique_urls = Object.keys(myset);
+
+      unique_urls = Object.keys(myset);
+
       for (index = 0; index < unique_urls.length; index++) {
-        var url = unique_urls[index];
+        url = unique_urls[index];
+
         (function(url) {
           chrome.history.getVisits({url: url}, function(visitItems) {
             var inner_index;
-            for (inner_index = 0; inner_index < visitItems.length; inner_index ++) {
+            for (inner_index = 0; inner_index < visitItems.length; inner_index++) {
               visit = visitItems[inner_index];
+
               (function(visit) {
-                var req = new XMLHttpRequest();
-                var fd = new FormData();
+                var req = new XMLHttpRequest(),
+                    fd = new FormData();
+
                 fd.append('url', url);
                 fd.append('visit_at', visit.visitTime);
 
-                chrome.identity.getProfileUserInfo(function (userInfo) {
+                chrome.identity.getProfileUserInfo(function(userInfo) {
                   fd.append('browser_id', userInfo.id);
                   fd.append('browser_type', "chrome");
+
                   req.open("POST", "http://10.104.92.195:3000/visits", true);
                   req.send(fd);
                 });
               })(visit);
+
             }
           });
         })(url);
