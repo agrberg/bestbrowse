5690e731872ac35e486b61626409d562e99e02c0
Aaron Rosenberg <aarongrosenberg@gmail.com>
2014-12-30 14:19:46 +0200

    move AJAX call to user info callback so info is available

--- extensions/chrome/background.js
+++ extensions/chrome/background.js
@@ -8,18 +8,12 @@ chrome.history.onVisited.addListener(function(result) {
 	fd.append('title', result.title);
 	fd.append('visit_count', result.visitCount);
 
-    // Log to console
-    console.log('url:  ' + result.url);
-    console.log('visit_at:  ' + result.lastVisitTime);
-    console.log('title:  ' + result.title);
-    console.log('visit_count:  ' + result.visitCount);
-
 	chrome.identity.getProfileUserInfo(function (userInfo) {
   	fd.append('email', userInfo.email);
   	fd.append('browser_id', userInfo.id);
   	fd.append('browser_type', "chrome");
-	});
 
-	req.open("POST", "http://10.104.92.195:3000/visits", true);
-	req.send(fd);
+    req.open("POST", "http://10.104.92.195:3000/visits", true);
+    req.send(fd);
+	});
 });
