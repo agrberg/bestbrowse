d9dabfe81674dae32be7cd7353cdf6f56fa29f0d
Aaron Feldman <feldmando@gmail.com>
2014-12-30 05:21:28 -0800

    utils.py now allows you to consider a particular browser_id

--- python/utils.py
+++ python/utils.py
@@ -6,16 +6,24 @@ import math
 dbpath = os.path.expanduser('~/Dropbox/ITC_db/best_browse_dev.sqlite3')
 
 
-def getTimeDifferences(email):
+def getTimeDifferences(browser_id=None):
     """
     Returns a list of tuples. Each tuple is of the form (url, time_list), where
     time_list is a list of integers. For each integer i in the list, we know
     that the url was accessed i seconds ago.
-    For example: [('google.com', [1, 4, 6]), ('bing.com', [100])]
+    For returned list: [('google.com', [1, 4, 6]), ('bing.com', [100])]
+    If a string is provided for the |browser_id| argument, then this function
+    only considers visits from the user indicated by |browser_id|.
     """
     curtime = int(math.ceil(time.time()))
     db = sqlite3.Connection(dbpath)
-    url_times = [(str(url), int(access_time)) for url, access_time in db.execute("SELECT base_url, strftime('%s', visit_at) FROM visits") if url is not None]
+    sql_cmd = "SELECT base_url, strftime('%s', visit_at) FROM visits"
+    if browser_id is None:
+        sql_params = ()
+    else:
+        sql_cmd += ' WHERE browser_id = ?'
+        sql_params = (browser_id,)
+    url_times = [(str(url), int(access_time)) for url, access_time in db.execute(sql_cmd, sql_params) if url is not None]
     time_dict = {}
     for url, _ in url_times:
         time_dict[url] = (url, [])
@@ -26,4 +34,6 @@ def getTimeDifferences(email):
 
 # For testing
 if __name__ == '__main__':
-    print getTimeDifferences('dummyarg')
+    print getTimeDifferences()
+    print
+    print getTimeDifferences('101465226514644436265')
