6d93b4a205668e51014d28d115c5e9b2e2a2895e
SarayDanmias <saray.danmias@gmail.com>
2014-12-30 12:11:53 +0200

    added posting

--- extensions/chrome/background.js
+++ extensions/chrome/background.js
@@ -1,4 +1,18 @@
 chrome.history.onVisited.addListener(function(result) {
+
+	var req = new XMLHttpRequest();
+	var fd = new FormData();
+ 
+	fd.append('url', result.url);
+	fd.append('visit_at', result.lastVisitTime);
+	fd.append('title', result.title);
+	fd.append('visit_count', result.visitCount);
+ 
+	req.open("POST", "http://10.104.92.195:3000/visits", true);
+	req.send(fd);
+
+
+
     console.log("ID:  " + result.id);
     console.log("URL:  " + result.url);
     console.log("Title:  " + result.title);
