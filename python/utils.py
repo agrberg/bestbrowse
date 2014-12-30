536e3d33051f9d8736bf91d662ac03df92e7f9fa
Aaron Feldman <feldmando@gmail.com>
2014-12-30 04:42:19 -0800

    function in utils now returns time measured in seconds ago

--- python/utils.py
+++ python/utils.py
@@ -1,5 +1,7 @@
 import sqlite3
 import os
+import time
+import math
 
 dbpath = os.path.expanduser('~/Dropbox/ITC_db/best_browse_dev.sqlite3')
 
@@ -8,18 +10,17 @@ def getTimeDifferences(email):
     """
     [("google.com", [1, 5, 9]), (...), ...]
     """
+    curtime = int(math.ceil(time.time()))
     db = sqlite3.Connection(dbpath)
-    url_times = [(str(url), int(time)) for url, time in db.execute("SELECT url, strftime('%s', visit_at) FROM visits")]
+    url_times = [(str(url), int(access_time)) for url, access_time in db.execute("SELECT url, strftime('%s', visit_at) FROM visits") if url is not None]
     time_dict = {}
     for url, _ in url_times:
-        if url is not None:
-            time_dict[url] = (url, [])
-    for url, time in url_times:
-        if url is not None:
-            time_dict[url][1].append(time)
+        time_dict[url] = (url, [])
+    for url, access_time in url_times:
+        time_dict[url][1].append(curtime - access_time)
     return time_dict.values()
 
 
 # For testing
 if __name__ == '__main__':
-    getTimeDifferences('dummyarg')
+    print getTimeDifferences('dummyarg')
