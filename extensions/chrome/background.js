dda9f673319647bad80573dce969fa865e874e80
Aaron Feldman <feldmando@gmail.com>
2014-12-30 02:35:30 -0800

    removed some logging from background.js and added permission for our server's IP address to manifest

--- extensions/chrome/background.js
+++ extensions/chrome/background.js
@@ -1,5 +1,5 @@
 chrome.history.onVisited.addListener(function(result) {
-
+    // Post data to server
 	var req = new XMLHttpRequest();
 	var fd = new FormData();
  
@@ -11,27 +11,9 @@ chrome.history.onVisited.addListener(function(result) {
 	req.open("POST", "http://10.104.92.195:3000/visits", true);
 	req.send(fd);
 
-
-
-    console.log("ID:  " + result.id);
-    console.log("URL:  " + result.url);
-    console.log("Title:  " + result.title);
-    console.log("Last visit time:  " + result.lastVisitTime);
-    console.log("Visit count:  " + result.visitCount);
-    console.log("Typed count:  " + result.typedCount);
-
-    chrome.history.getVisits({url: result.url}, function(visitItems) {
-        console.log("Number of visits to this URL:  " + visitItems.length);
-        console.log("Listing visits to this URL:");
-        var index;
-        for (index = 0; index < visitItems.length; index ++) {
-            visit = visitItems[index]
-            console.log("");
-            console.log("URL ID:  " + visit.id);
-            console.log("Visit ID:  " + visit.visitId);
-            console.log("Visit time:  " + visit.visitTime);
-            console.log("Referring visit ID:  " + visit.referringVisitId);
-            console.log("Transition:  " + visit.transition);
-        }
-    });
+    // Log to console
+    console.log('url:  ' + result.url);
+    console.log('visit_at:  ' + result.lastVisitTime);
+    console.log('title:  ' + result.title);
+    console.log('visit_count:  ' + result.visitCount);
 });
