4fb0721d67919bca72c679a4d4b9d021dfed659d
Aaron Feldman <feldmando@gmail.com>
2014-12-30 01:25:58 -0800

    now also logs information from VisitItems

--- extensions/chrome/background.js
+++ extensions/chrome/background.js
@@ -1,6 +1,23 @@
 chrome.history.onVisited.addListener(function(result) {
+    console.log("ID:  " + result.id);
     console.log("URL:  " + result.url);
     console.log("Title:  " + result.title);
     console.log("Last visit time:  " + result.lastVisitTime);
     console.log("Visit count:  " + result.visitCount);
-}); 
+    console.log("Typed count:  " + result.typedCount);
+
+    chrome.history.getVisits({url: result.url}, function(visitItems) {
+        console.log("Number of visits to this URL:  " + visitItems.length);
+        console.log("Listing visits to this URL:");
+        var index;
+        for (index = 0; index < visitItems.length; index ++) {
+            visit = visitItems[index]
+            console.log("");
+            console.log("URL ID:  " + visit.id);
+            console.log("Visit ID:  " + visit.visitId);
+            console.log("Visit time:  " + visit.visitTime);
+            console.log("Referring visit ID:  " + visit.referringVisitId);
+            console.log("Transition:  " + visit.transition);
+        }
+    });
+});
