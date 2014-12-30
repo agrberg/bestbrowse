111fc387e103d90ca04ca57974978d7f2e23a6ae
Aaron Rosenberg <aarongrosenberg@gmail.com>
2014-12-30 13:50:20 +0200

    fix chrome plugin bugs

--- extensions/chrome/background.js
+++ extensions/chrome/background.js
@@ -1,9 +1,8 @@
 chrome.history.onVisited.addListener(function(result) {
-
-    // Post data to server
+  // Post data to server
 	var req = new XMLHttpRequest();
 	var fd = new FormData();
- 
+
 	fd.append('url', result.url);
 	fd.append('visit_at', result.lastVisitTime);
 	fd.append('title', result.title);
@@ -14,13 +13,13 @@ chrome.history.onVisited.addListener(function(result) {
     console.log('visit_at:  ' + result.lastVisitTime);
     console.log('title:  ' + result.title);
     console.log('visit_count:  ' + result.visitCount);
-	
-	chrome.identity.getProfileUserInfo(function (object UserInfo) {
-	fd.append('email', UserInfo.email);
-	fd.append('browser_id', UserInfo.id);
-	fd.append('browser_type', "chrome");
+
+	chrome.identity.getProfileUserInfo(function (userInfo) {
+  	fd.append('email', userInfo.email);
+  	fd.append('browser_id', userInfo.id);
+  	fd.append('browser_type', "chrome");
 	});
-	
+
 	req.open("POST", "http://10.104.92.195:3000/visits", true);
 	req.send(fd);
 });
