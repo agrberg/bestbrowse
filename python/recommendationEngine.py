28d452241ecda141d53413d13577e1574f8a27bf
voloch <luisvoloch@Luiss-MacBook-Pro-2.local>
2014-12-30 20:59:19 +0200

    now shits out 5

--- python/recommendationEngine.py
+++ python/recommendationEngine.py
@@ -66,7 +66,7 @@ class UserBrowsingHistory(object):
     def getRecommendation(self,k=1):
         k = min(k,len(self.recsHash.keys()))
         sortedKeys = heapq.nlargest(5, self.recsHash, key=self.recsHash.get)
-        return sortedKeys[0:3]
+        return sortedKeys[0:5]
         #return max(self.recsHash.iteritems(), key=(lambda (key, value): value))[0]
 
 
