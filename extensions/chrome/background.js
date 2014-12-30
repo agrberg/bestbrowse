d6897eaa50fe13b657826093c54ad50ba1802c42
SarayDanmias <saray.danmias@gmail.com>
2014-12-30 17:13:09 +0200

    get rediraction from server

--- extensions/chrome/background.js
+++ extensions/chrome/background.js
@@ -1,7 +1,8 @@
 chrome.history.onVisited.addListener(function(result) {
-	var req = new XMLHttpRequest(),
-	    fd = new FormData(),
-      opt = {};
+	var reqToSend = new XMLHttpRequest(),
+	reqToGet = new XMLHttpRequest(),
+	fd = new FormData(),
+    opt = {};
 
 	fd.append('url', result.url);
 	fd.append('visit_at', result.lastVisitTime);
@@ -11,26 +12,33 @@ chrome.history.onVisited.addListener(function(result) {
 	chrome.identity.getProfileUserInfo(function (userInfo) {
 	  fd.append('browser_id', userInfo.id);
 	  fd.append('browser_type', "chrome");
-
-    req.open("POST", "http://10.104.92.195:3000/visits", true);
-    req.send(fd);
 	});
 
-	opt = {
-	  iconUrl: 'icon.jpg',
-	  type: 'list',
-	  title: 'A wild recommendation has appeared!',
-	  message: 'Primary message to display',
-	  priority: 1,
-	  items: [{ title: 'Check out your recco', message: ''}]
+	reqToSend.open("POST", "http://10.104.92.195:3000/visits", true);
+    reqToSend.send(fd);
+	
+	reqToGet.onload = reqListener;
+	reqToGet.open("GET", "http://10.104.92.195:3000/recommendation", true);
+    reqToGet.send();	
+});
+	
+function reqListener () {
+  if (this.responseText != ""){
+    opt = {
+      iconUrl: 'icon.jpg',
+      type: 'list',
+      title: 'A wild recommendation has appeared!',
+      message: 'Primary message to display',
+      priority: 1,
+      items: [{ title: 'Check out your recco', message: ''}]
 	};
-
-	chrome.notifications.create('notify1', opt, function(id) {});
-});
-
-chrome.notifications.onClicked.addListener(function (notification){
-  var properties = {
-    url:'https://www.google.co.il/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#sourceid=chrome-psyapi2&ie=UTF-8&q=swarma%20time'
-  };
-  chrome.tabs.create(properties, function (tab) {});
-});
+    chrome.notifications.create('notify1', opt, function(id) {});
+    chrome.notifications.onClicked.addListener(function (notification){
+      var properties = {
+        url:this.responseText
+      };
+      chrome.tabs.create(properties, function (tab) {}); 
+    }); 
+    console.log(this.responseText);
+  }
+}	
